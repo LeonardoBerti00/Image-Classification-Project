45a40be6ad4232719beaa3e959bb130c3eb73f5d
LeonardoBerti07 <98135135+LeonardoBerti07@users.noreply.github.com>
2022-06-12 18:43:14 +0200

    Update CNNmodels.py

--- Simple CNN/CNNmodels.py
+++ Simple CNN/CNNmodels.py
@@ -24,59 +24,6 @@ class Net(nn.Module):
         return x
 
 
-class vgg16(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.vgg = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, 1),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(256, 512, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, 1),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2, stride=2),
-
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, 3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, 1),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2, stride=2))
-
-        self.dense = nn.Sequential(
-            nn.Linear(14336, 4096),
-            nn.ReLU(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(),
-            nn.Linear(4096, 8),
-            # nn.ReLU()
-        )
 
     def forward(self, x):
         out = self.vgg(x)
